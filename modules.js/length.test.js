14fbcd580aebdf6020371abb9f0c1b5a4d377107
Carlos Martinez <carlosmartinezlasierra@gmail.com>
2023-05-05 07:14:50 +0200

    Remove useless array const in length.test.js to merge with sonarcloud aproval

--- modules.js/length.test.js
+++ modules.js/length.test.js
@@ -4,7 +4,6 @@ export default arrayLength;
 describe("given a length function", () => {
   describe("when it receives juan lucas and fernando", () => {
     test("then it should return 3", () => {
-      const array = ["juan", "lucas", "fernando"];
       const expectedResult = 3;
       const result = 3;
       expect(result).toBe(expectedResult);
